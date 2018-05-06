e8aca721f4c33dbb400adbe9c277dd2cb4e4a6f6
Luffykun <harsheth96@gmail.com>
2018-05-06 12:31:09 +0530

    Finished untested MLE code

--- mle.cpp
+++ mle.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
+#include <cstring>
 #include <stdlib.h>
 #include <lapacke.h>
 #include <cblas.h>
+#include <math.h>
 
 using namespace std;
 
 void AR(int n, double* Id, double* B, double* result, float phi_1)
 {
     int info;
-    memcpy(result, B, n*n;
+    memcpy(result, B, n*n);
     info = LAPACKE_dlascl(LAPACK_ROW_MAJOR, 'L', 1, 1, -1, phi_1, n, n, result, n);
     if(info != 0)
     {
         cout<<"Error in scaling of B";
         exit;
     }
-    clbas_daxpy(n*n, 1, Id, 1, result, 1);
+    cblas_daxpy(n*n, 1, Id, 1, result, 1);
     return;
 }
 
@@ -34,7 +36,7 @@ void differential(int n, double *Id, double *B, double *result, float d)
     }
     double *B_pow = new double[n*n];
     memcpy(B_pow, B, n*n);
-    memcpy(result, Id, n*n)
+    memcpy(result, Id, n*n);
     double coeff = 1.0;
     for(int j=0; j<iter; j++)
     {
@@ -64,17 +66,27 @@ double calc_MLE(double *y, int n, float var, float d, float phi_1)
     }
     double *temp = new double[n*n];
     double *result = new double[n*n];
-
+    double *z = new double[n];
     //Load differential to result
     differential(n, Id, B, result, d);
 
     //Load AR to temp
-    AR(n, Id, B, temp, phi_1)
+    AR(n, Id, B, temp, phi_1);
 
     //Compute AR*differential
     cblas_dtrmm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasNonUnit, n, n, 1, temp, n, result, n);
+
+    //Multiply the resulting operator with y to get z
+    memcpy(z, y, n);
+    cblas_dtrmv(CblasRowMajor, CblasLower, CblasNoTrans, CblasNonUnit, n, result, n, z, 1);
+
+    double mle = 1;
+    for(int i = 0; i < n; i++)
+    {
+        mle = mle * 1/sqrt(2*M_PI*var)*exp(-1*z[i]*z[i]/var);
+    }
     
-    return 0.0;
+    return mle;
 }
 
 int main()
@@ -84,6 +96,7 @@ int main()
     //calc_MLE(a,3,c,d,e);
     double a[] = {1,0,4,1};
     double b[] = {1,0,8,4};
+    double x[] = {1,2}; 
     double *c = new double[4];
     double *result = new double[4];
     for(int i = 0; i < 4; i++) result[i] = 0;
@@ -103,6 +116,11 @@ int main()
             cout<<c[j + 2*i]<<" ";
         }
         cout<<endl;
+    }
+    cblas_dtrmv(CblasRowMajor, CblasLower, CblasNoTrans, CblasNonUnit, 2, a, 2, x, 1);
+    for(int i = 0; i < 2; i++)
+    {
+        cout<<x[i]<<endl;
     }
     return 0;
 }
